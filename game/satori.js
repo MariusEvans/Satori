5f9ee471235605568a611db49cee812a103bd93d
OMC <54768583+OMC-gd@users.noreply.github.com>
2019-09-13 19:12:26 +0100

    Add files via upload
    All images changed (players and enemies are now 64x64, thrust 63x32, laser 32x32). All code amended to take this into account. ShowDebug function re-written. Enemy laser now set to approx once every 100 frames where their X axis is below 1000 (to avoid lasers coming from unexpected players at the far edge). Hit detection added for enemy lasers. Thrust drawing now redone so it does not lag behind player movement.

--- game/satori.js
+++ game/satori.js
@@ -48,22 +48,22 @@ var enemyExplosions = [];
 
 //---------- ASSETS
 //backgrounds
-var starfield = newBackground('../assets/backgrounds/space.jpg');
+var starfield = newBackground('../assets/gfx/background1.png');
 //ships
 const TOTAL_SHIPS = 2; //total number of ship assets
-var supernova = newShip('../assets/spaceships/supernova/supernova.png',128,128);
-var phoenix = newShip('../assets/spaceships/phoenix/phoenix.png',128,128);
+var supernova = newShip('../assets/gfx/player1.png',65,65);
+var phoenix = newShip('../assets/gfx/player2.png',65,65);
 phoenix.y=canvas.width/4; //change y of player 2 so not overlapping with player 1
 var total = ["supernova","phoenix"];
 //lasers
-var singleP1 = newLaser('../assets/lasers/single.png',5,5);
-var singleP2 = newLaser('../assets/lasers/single.png',5,5);
-var singleEnemy = newLaser('../assets/lasers/single.png',5,5);
+var singleP1 = newLaser('../assets/gfx/laser.png',16,16);
+var singleP2 = newLaser('../assets/gfx/laser.png',16,16);
+var singleEnemy = newLaser('../assets/gfx/laser.png',16,16);
 //exhausts
-var supernova_exhaust = newExhaust("../assets/spaceships/supernova/Exhaust/Normal_flight/Exhaust1/exhaust4.png");
-var phoneix_exhaust = newExhaust("../assets/spaceships/phoenix/Exhaust/Normal_flight/Exhaust1/exhaust4.png")
+var supernova_exhaust = newExhaust("../assets/gfx/thrust.png");
+var phoneix_exhaust = newExhaust("../assets/gfx/thrust.png");
 //enemies
-var enemy_1 = newEnemy("../assets/enemies/enemy-1.png",300,300);
+var enemy_1 = newEnemy('../assets/gfx/enemy1.png',65,65);
 //sounds
 var fxShot = new Sound("../assets/sounds/laser.m4a",5,1);
 var fxExplodeP1 = new Sound("../assets/sounds/explode.m4a");
@@ -72,9 +72,9 @@ var fxHit = new Sound("../assets/sounds/hit.m4a",5);
 var fxThrust = new Sound("../assets/sounds/thrust.m4a",1,0.5);
 var fxThrustP2 = new Sound("../assets/sounds/thrust.m4a",1,0.5);
 //explosions
-var explosionP1 = newExplosion("../assets/explosions/explosionP1/explosionP1_5.png");
-var explosionP2 = newExplosion("../assets/explosions/explosionP1/explosionP1_5.png");
-var explosionEnemy = newExplosion("../assets/explosions/explosionP1/explosionP1_5.png");
+var explosionP1 = newExplosion("../assets/gfx/explosion.png");
+var explosionP2 = newExplosion("../assets/gfx/explosion.png");
+var explosionEnemy = newExplosion("../assets/gfx/explosion.png");
 
 //images
 var starfieldImg = new Image(); starfieldImg.src = starfield.url;
@@ -87,6 +87,7 @@ var enemy_1Img = new Image(); enemy_1Img.src = enemy_1.url;
 var explosionP1Img = new Image(); explosionP1Img.src = explosionP1.url;
 var explosionP2Img = new Image(); explosionP2Img.src = explosionP2.url;
 var explosionEnemyImg = new Image(); explosionEnemyImg.src = explosionEnemy.url;
+var laserImg = new Image(); laserImg.src = singleP1.url;
 
 //count time to clear level + blinking for PRESS ENTER
 window.setInterval(function()
@@ -243,6 +244,7 @@ function Satori()
             drawLives(); //draw lives, 1UP, Score, PRESS ENTER
             explodePlayer1(); //draw explosion for P1
             explodePlayer2(); //draw explosion for P2
+            showDebug(); //display hitboxes etc
         }
         else
         {
@@ -327,7 +329,7 @@ function Satori()
                         console.log("New player.");
                         twoPlayer=true;
                         livesP2 = MAX_LIVES;
-                        phoenix = newShip('../assets/spaceships/phoenix/phoenix.png',128,128);
+                        phoenix = newShip('../assets/gfx/player2.png', 65, 65);
                         phoenix.y=canvas.width/4; //change y of player 2 so not overlapping with player 1
                     }
                 }
@@ -401,8 +403,6 @@ function Satori()
 function drawBackground() {
     context.drawImage(starfieldImg,starfield.x,starfield.y,canvas.width,canvas.height);
     context.drawImage(starfieldImg,starfield.x + canvas.width,starfield.y,canvas.width,canvas.height);
-    
-    showDebug("Rect",starfield.x + canvas.width, 0, 2, 704, 0);
 
     if(starfield.x <= (canvas.width * -1)) {
         starfield.x += canvas.width;
@@ -420,7 +420,7 @@ function createLasersPlayer1() {
     
     if (singleP1.lasers.length<=MAX_SHOTS && explodingP1 == false && livesP1>0) {
         fxShot.play();
-        singleP1.lasers.push([supernova.x + supernova.width - 45, supernova.y + (supernova.height/2)-2, singleP1.width,   singleP1.height]);
+        singleP1.lasers.push([supernova.x + 16, supernova.y + 45, singleP1.width, singleP1.height]);
     }
 }
 
@@ -432,7 +432,7 @@ function createLasersPlayer2() {
     
     if (singleP2.lasers.length<=MAX_SHOTS && explodingP2 == false && livesP2>0) {
         fxShot.play();
-        singleP2.lasers.push([phoenix.x + phoenix.width - 35, phoenix.y + (phoenix.height/2)+1, singleP2.width, singleP2.height]);
+        singleP2.lasers.push([phoenix.x + 16, phoenix.y + 45, singleP2.width, singleP2.height]);
     }
 }
 
@@ -443,7 +443,6 @@ function drawPlayer1(){
         //move ship
         if (rightKeyP1){ //draw exhaust when moving forward
             fxThrust.play();
-            context.drawImage(supernovaExhaustImg,supernova.x+supernova.width/2 -55,supernova.y+supernova.height/2 -15);
             supernova.x += SHIP_SPEED;
         }
         else if (leftKeyP1){supernova.x -= SHIP_SPEED;}
@@ -458,6 +457,9 @@ function drawPlayer1(){
         if ((supernova.y + supernova.height + 100) >= canvas.height){supernova.y = canvas.height - supernova.height - 100;}
         
         if (blinkOn) {
+            if (rightKeyP1) {
+                context.drawImage(supernovaExhaustImg, supernova.x - 50, supernova.y + 18 + (Math.random() * 5));
+            }
             context.drawImage(supernovaImg,supernova.x,supernova.y);
         }
     }
@@ -482,7 +484,6 @@ function drawPlayer2(){
         //move ship
         if (rightKeyP2){ //draw exhaust when moving forward
             fxThrustP2.play();
-            context.drawImage(phoenixExhaustImg,phoenix.x+phoenix.width/2 -65,phoenix.y+phoenix.height/2 -15);
             phoenix.x += SHIP_SPEED;
         }
         else if (leftKeyP2){phoenix.x -= SHIP_SPEED;}
@@ -497,6 +498,9 @@ function drawPlayer2(){
         if ((phoenix.y + phoenix.height + 100) >= canvas.height){phoenix.y = canvas.height - phoenix.height - 100;}
         
         if (blinkOn) {
+            if (rightKeyP2) {
+                context.drawImage(phoenixExhaustImg, phoenix.x - 50, phoenix.y + 18 + (Math.random() * 5));
+            }
             context.drawImage(phoenixImg,phoenix.x,phoenix.y);
         }
     }
@@ -515,10 +519,6 @@ function drawPlayer2(){
 }
 function drawLaserPlayer1(){
     if(livesP1>0){
-        // var singleP1Img = new Image();
-        // singleP1Img.src = singleP1.url;
-        //context.drawImage(singleP1Img,singleP1.x,singleP1.y);
-
         //move laser
         if(singleP1.lasers.length>0)
         {
@@ -539,8 +539,7 @@ function drawLaserPlayer1(){
         if(singleP1.lasers.length>0)
         {
             for (var i = 0; i < singleP1.lasers.length; i++) {
-                context.fillStyle = 'red';
-                context.fillRect(singleP1.lasers[i][0],singleP1.lasers[i][1],singleP1.lasers[i][2],singleP1.lasers[i][3]);
+                context.drawImage(laserImg, singleP1.lasers[i][0], singleP1.lasers[i][1]);
             }
         } 
     }
@@ -548,10 +547,6 @@ function drawLaserPlayer1(){
 function drawLaserPlayer2(){
     if(twoPlayer && livesP2>0)
     {
-        // var singleP1Img = new Image();
-        // singleP1Img.src = singleP1.url;
-        //context.drawImage(singleP1Img,singleP1.x,singleP1.y);
-
         //move laser
         if(singleP2.lasers.length>0)
         {
@@ -571,17 +566,16 @@ function drawLaserPlayer2(){
         if(singleP2.lasers.length>0)
         {
             for (var i = 0; i < singleP2.lasers.length; i++) {
-                context.fillStyle = 'red';
-                context.fillRect(singleP2.lasers[i][0],singleP2.lasers[i][1],singleP2.lasers[i][2],singleP2.lasers[i][3]);
+                context.drawImage(laserImg, singleP2.lasers[i][0], singleP2.lasers[i][1]);
             }
         } 
     }
 }
 function drawLaserEnemy(){
     //create lasers
-    if(time%2==0 && enemy_1.enemies.length>0){
-        for(var i = 0; i<enemy_1.enemies.length; i++){
-            singleEnemy.lasers.push([enemy_1.enemies[i][0] + enemy_1.enemies[i][2]-300, enemy_1.enemies[i][1] + (enemy_1.enemies[i][3]/2)-120, singleEnemy.width, singleEnemy.height]);
+    for(var i = 0; i<enemy_1.enemies.length; i++){
+        if (Math.floor(Math.random() * 100) == 0 && enemy_1.enemies[i][0] < 1000) {
+            singleEnemy.lasers.push([enemy_1.enemies[i][0] + 32, enemy_1.enemies[i][1] + 32, 16, 16]);
         }
     }
 
@@ -604,8 +598,7 @@ function drawLaserEnemy(){
     if(singleEnemy.lasers.length>0)
     {
         for (var i = 0; i < singleEnemy.lasers.length; i++) {
-            context.fillStyle = 'red';
-            context.fillRect(singleEnemy.lasers[i][0],singleEnemy.lasers[i][1],singleEnemy.lasers[i][2],singleEnemy.lasers[i][3]);
+            context.drawImage(laserImg, singleEnemy.lasers[i][0], singleEnemy.lasers[i][1]);
         }
     } 
 }
@@ -630,7 +623,7 @@ function drawLives()
     shipImg.src = supernova.url;
     for(var i = 0; i<livesP1; i++)
     {
-        context.drawImage(shipImg,20*(i*2),10,supernova.width/2,supernova.height/2);
+        context.drawImage(shipImg, 20 + (40 * i), 40, supernova.width/2, supernova.height/2);
     }
 
     //PLAYER 2
@@ -649,7 +642,7 @@ function drawLives()
         //LIVES
         for(var i = 0; i<livesP2; i++)
         {
-            context.drawImage(shipImg,20*(i*2.5) + (canvas.width-180),10,phoenix.width/2,phoenix.height/2);
+            context.drawImage(shipImg, (40 * i) + 1140,40,phoenix.width/2,phoenix.height/2);
         }
     }
 }
@@ -674,9 +667,7 @@ function drawEnemies()
     if(enemy_1.enemies.length>0)
     {
         for (var i = 0; i < enemy_1.enemies.length; i++) {
-            context.drawImage(enemy_1Img,enemy_1.enemies[i][0],enemy_1.enemies[i][1],enemy_1.enemies[i][2]/5,enemy_1.enemies[i][3]/5);
-            
-            showDebug("Arc",enemy_1.enemies[i][0] + enemy_1.enemies[i][2]/10,enemy_1.enemies[i][1] + enemy_1.enemies[i][3]/10, 0, 0, enemy_1.enemies[i][2]/10);
+            context.drawImage(enemy_1Img,enemy_1.enemies[i][0],enemy_1.enemies[i][1]);
         }
     } 
 }
@@ -691,8 +682,8 @@ function drawEnemyExplosions()
             continue;
         }
         context.drawImage(explosionEnemyImg,
-                          enemyExplosions[i][0] + ((Math.random() * 30) - 15),
-                          enemyExplosions[i][1] + ((Math.random() * 30) - 15));
+                          enemyExplosions[i][0] + ((Math.random() * 20) - 10),
+                          enemyExplosions[i][1] + ((Math.random() * 20) - 10));
     }
 }
 
@@ -703,12 +694,12 @@ function hitTestPlayer1()
         {
             for (var j = 0; j < enemy_1.enemies.length; j++) 
             {                
-                if (circlesCollide(singleP1.lasers[i][0] + 2.5, singleP1.lasers[i][1] + 2.5, 5,
-                    enemy_1.enemies[j][0] + ((enemy_1.enemies[j][2] / 5) / 2),
-                    enemy_1.enemies[j][1] + ((enemy_1.enemies[j][3] / 5) / 2),
-                    (enemy_1.enemies[j][2] / 5) / 2) == true) {
+                if (circlesCollide(singleP1.lasers[i][0] + 8, singleP1.lasers[i][1] + 8, 8,
+                    enemy_1.enemies[j][0] + 32,
+                    enemy_1.enemies[j][1] + 32,
+                    32) == true) {
                     
-                    enemyExplosions.push([enemy_1.enemies[j][0] - 32, enemy_1.enemies[j][1] - 32, (SHIP_EXPLODE_DUR * FPS) / 2]);
+                    enemyExplosions.push([enemy_1.enemies[j][0], enemy_1.enemies[j][1], (SHIP_EXPLODE_DUR * FPS) / 2]);
                     
                     scoreP1 += 70;
                     fxHit.play();
@@ -729,12 +720,12 @@ function hitTestPlayer2()
         {
             for (var j = 0; j < enemy_1.enemies.length; j++) 
             {
-                if (circlesCollide(singleP2.lasers[i][0] + 2.5, singleP2.lasers[i][1] + 2.5, 5,
-                    enemy_1.enemies[j][0] + ((enemy_1.enemies[j][2] / 5) / 2),
-                    enemy_1.enemies[j][1] + ((enemy_1.enemies[j][3] / 5) / 2),
-                    (enemy_1.enemies[j][2] / 5) / 2) == true) {
+                if (circlesCollide(singleP2.lasers[i][0] + 8, singleP2.lasers[i][1] + 8, 8,
+                    enemy_1.enemies[j][0] + 32,
+                    enemy_1.enemies[j][1] + 32,
+                    32) == true) {
                     
-                    enemyExplosions.push([enemy_1.enemies[j][0] - 32, enemy_1.enemies[j][1] - 32, (SHIP_EXPLODE_DUR * FPS) / 2]);
+                    enemyExplosions.push([enemy_1.enemies[j][0], enemy_1.enemies[j][1], (SHIP_EXPLODE_DUR * FPS) / 2]);
                     
                     scoreP2 += 70;
                     fxHit.play();
@@ -752,14 +743,11 @@ function shipCollisionPlayer1()
     if(!INVINCIBLE && livesP1>0 && supernova.blinkNo==0 && explodingP1==false)
     {
         for (var j = 0; j < enemy_1.enemies.length; j++) 
-        {
-            showDebug("Arc",supernova.x + (supernova.width / 2),supernova.y + (supernova.height / 2),0,0,supernova.width / 4);
-            
-            if (circlesCollide(supernova.x + (supernova.width / 2),
-                supernova.y + (supernova.height / 2), supernova.width / 4,
-                enemy_1.enemies[j][0] + ((enemy_1.enemies[j][2] / 5) / 2),
-                enemy_1.enemies[j][1] + ((enemy_1.enemies[j][3] / 5) / 2),
-                (enemy_1.enemies[j][2] / 5) / 2) == true && explodingP1 == false) {
+        {   
+            if (circlesCollide(supernova.x + 32, supernova.y + 32, 32,
+                enemy_1.enemies[j][0] + 32,
+                enemy_1.enemies[j][1] + 32,
+                32) == true && explodingP1 == false) {
 
                 fxExplodeP1.play();
                 explodingP1 = true;
@@ -772,6 +760,19 @@ function shipCollisionPlayer1()
                 j--;
             }
         }
+        
+        for (var i = 0; i < singleEnemy.lasers.length; i++) {
+            if (circlesCollide(supernova.x + 32, supernova.y + 32, 32,
+                singleEnemy.lasers[i][0] + 8, singleEnemy.lasers[i][1] + 8, 8) == true && explodingP1 == false) {
+                fxExplodeP1.play();
+                explodingP1 = true;
+                explosionP1.x = supernova.x;
+                explosionP1.y = supernova.y;
+                explosionP1.displayCountdown = (SHIP_EXPLODE_DUR * FPS);
+                livesP1--;
+            }
+        }
+        
     }    
 }
 function shipCollisionPlayer2()
@@ -780,13 +781,10 @@ function shipCollisionPlayer2()
     {
         for (var j = 0; j < enemy_1.enemies.length; j++) 
         {
-            showDebug("Arc",phoenix.x + (phoenix.width / 2),phoenix.y + (phoenix.height / 2),0,0,phoenix.width / 4);
-                
-            if (circlesCollide(phoenix.x + (phoenix.width / 2),
-                phoenix.y + (phoenix.height / 2), phoenix.width / 4,
-                enemy_1.enemies[j][0] + ((enemy_1.enemies[j][2] / 5) / 2),
-                enemy_1.enemies[j][1] + ((enemy_1.enemies[j][3] / 5) / 2),
-                (enemy_1.enemies[j][2] / 5) / 2) == true) {
+            if (circlesCollide(phoenix.x + 32, phoenix.y + 32, 32,
+                enemy_1.enemies[j][0] + 32,
+                enemy_1.enemies[j][1] + 32,
+                32) == true) {
                 
                 fxExplodeP2.play();
                 explodingP2 = true;
@@ -799,6 +797,19 @@ function shipCollisionPlayer2()
                 j--;
             }
         }
+        
+        for (var i = 0; i < singleEnemy.lasers.length; i++) {
+            if (circlesCollide(phoenix.x + 32, phoenix.y + 32, 32,
+                singleEnemy.lasers[i][0] + 8, singleEnemy.lasers[i][1] + 8, 8) == true && explodingP2 == false) {
+                fxExplodeP2.play();
+                explodingP2 = true;
+                explosionP2.x = phoenix.x;
+                explosionP2.y = phoenix.y;
+                explosionP2.displayCountdown = (SHIP_EXPLODE_DUR * FPS);
+                livesP2--;
+            }
+        }
+        
     }
 }
 function checkGameOver()
@@ -833,7 +844,7 @@ function explodePlayer1()
     
     if (explosionP1.displayCountdown <= 0) {
         explodingP1 = false;
-        supernova = newShip('../assets/spaceships/supernova/supernova.png',128,128);
+        supernova = newShip('../assets/gfx/player1.png', 65, 65);
     }
 }
 function explodePlayer2()
@@ -845,7 +856,7 @@ function explodePlayer2()
     
     if (explosionP2.displayCountdown <= 0) {
         explodingP2 = false;
-        phoenix = newShip('../assets/spaceships/phoenix/phoenix.png',128,128);
+        phoenix = newShip('../assets/gfx/player2.png', 65, 65);
     }
 }
 
@@ -853,27 +864,48 @@ function circlesCollide(firstX, firstY, firstR, secondX, secondY, secondR) {
     if (Math.sqrt(Math.pow(firstX - secondX, 2) + Math.pow(firstY - secondY, 2)) < firstR + secondR) return true;
     return false;
 }
-function showDebug(Shape, X,Y,W,H,R)
+function showDebug()
 {
-    var x = X, y = Y;
-    var shape = Shape;
-    var width = W, height = H;
-    var radius = R;
-
-    if(showWorkings==1){
-        context.fillStyle = "lightgreen";
-        
-        if(shape=="Rect"){ //draw background debug line
-            context.fillRect(x, y, width, height);
-        }
-        if(shape=="Arc"){ //draw debug circles around ships + enemies
-            context.beginPath();
-            context.arc(x,
-                        y,
-                        radius, 0, 2*Math.PI);
-            context.fill();
-        }
+    if (showWorkings == -1) return;
+    context.fillStyle = "white";
+    context.fillRect(starfield.x + canvas.width, 0, 2, 720);
+    
+    context.strokeStyle = "magenta";
+    context.lineWidth = 2;
+    for (var i = 0; i < singleP1.lasers.length; i++)  {
+        context.beginPath();
+        context.arc(singleP1.lasers[i][0] + 8, singleP1.lasers[i][1] + 8, 8, 0, 2*Math.PI);
+        context.stroke();
     }
+    
+    for (var i = 0; i < singleP2.lasers.length; i++)  {
+        context.beginPath();
+        context.arc(singleP2.lasers[i][0] + 8, singleP2.lasers[i][1] + 8, 8, 0, 2*Math.PI);
+        context.stroke();
+    }
+    
+    for (var i = 0; i < singleEnemy.lasers.length; i++) {
+        context.beginPath();
+        context.arc(singleEnemy.lasers[i][0] + 8, singleEnemy.lasers[i][1] + 8, 8, 0, 2*Math.PI);
+        context.stroke();
+    }
+    
+    context.strokeStyle = "cyan";
+    for (var i = 0; i < enemy_1.enemies.length; i++)  {
+        context.beginPath();
+        context.arc(enemy_1.enemies[i][0] + 32, enemy_1.enemies[i][1] + 32, 32, 0, 2*Math.PI);
+        context.stroke();
+    }
+    
+    context.strokeStyle = "lightgreen";
+    context.beginPath();
+    context.arc(supernova.x + 32, supernova.y + 32, 32, 0, 2*Math.PI);
+    context.stroke();
+    
+    context.strokeStyle = "yellow";
+    context.beginPath();
+    context.arc(phoenix.x + 32, phoenix.y + 32, 32, 0, 2*Math.PI);
+    context.stroke();
 }
 
 function animate() 
@@ -889,7 +921,7 @@ function animate()
         context.fillStyle = "white";
         context.fillText("PAUSED",canvas.width/2.25,canvas.height/2);
 
-        context.font = "30px Courier New";
-        context.fillText("PRESS P TO UNPAUSE",canvas.width/2.5 -5,canvas.height/2 + 40);
+        //context.font = "30px Courier New";
+        //context.fillText("PRESS P TO UNPAUSE",canvas.width/2.5 -5,canvas.height/2 + 40);
     }
 }
